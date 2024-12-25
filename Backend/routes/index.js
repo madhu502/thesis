4a60a6cf4e65a9890acdd5044d459a58f0a794df
Madhu Chaudhary <madhu@MacBook-Air-3.local>
2024-12-25 12:25:51 +0545

    authentication

--- Backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const userSignInController = require("../controller/user/userSignIn");
-
-module.exports = router;
---
+++ Backend/routes/routes.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const router = express.Router();
+
+const userSignInController = require("../controller/user/userSignIn");
+const userSignUpController = require("../controller/user/userSignUp");
+const userDetailsController = require('../controller/user/userDetails')
+const authToken = require('../middleware/authToken')
+const userLogout = require('../controller/user/userLogout')
+const allUsers = require('../controller/user/allUser')
+const updateUser = require('../controller/user/updateUser')
+
+
+
+
+
+
+router.post("/signup",userSignUpController)
+router.post("/signin",userSignInController)
+router.get("/user-details",authToken,userDetailsController)
+router.get("/userLogout",userLogout)
+
+//admin panel 
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
+
+module.exports = router;
+
