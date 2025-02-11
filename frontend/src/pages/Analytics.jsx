f5b9ddf41dc769c50faedee5142d299363e6dee1
Madhu Chaudhary <madhu@MacBook-Air-3.local>
2025-02-11 20:55:16 +0545

    changes

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -6,7 +6,7 @@ const Analytics = () => {
     const initViz = () => {
       const containerDiv = document.getElementById("vizContainer");
       const url =
-        "https://public.tableau.com/views/Marketbasketanalysis_17180361008960/Sheet1?:language=en-GB&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
+        "https://public.tableau.com/views/RetailSpaceAnalytics/Dashboard1?:language=en-GB&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link";
 
       if (window.tableau) {
         new window.tableau.Viz(containerDiv, url);
@@ -21,7 +21,7 @@ const Analytics = () => {
       <div className="bg-white py-2 px-4 flex justify-between items-center">
         <h1 className="font-bold text-lg">Analytics</h1>
       </div>
-      <div id="vizContainer" style={{ width: "100%", height: "700px" }}></div>
+      <div id="vizContainer" style={{ width: "100%", height: "900px" }}></div>
     </div>
   );
 };
